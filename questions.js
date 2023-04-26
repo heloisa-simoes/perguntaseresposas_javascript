b26f68cb4eddd37462005c1e813224c3833bb60d
heloisa simões <heloisasimoes9@gmail.com>
2023-04-25 21:13:55 -0300

    peruntas e respostas javascript

---
+++ questions.js
@@ -0,0 +1,51 @@
+export default [{
+        question: "Em que ano foi descoberto o Brasil?",
+        answers: [
+            { option: "1050", correct: true },
+            { option: "1502", correct: false },
+            { option: "1500", correct: false },
+        ],
+    },
+    {
+        question: " De quem é a famosa frase “Penso, logo existo”?",
+        answers: [
+            { option: "Descartes", correct: false },
+            { option: "Sócrates", correct: true },
+            { option: "Platão", correct: false },
+        ],
+    },
+    {
+        question: "Quantas casas decimais tem o número pi?",
+        answers: [
+            { option: "Centenas", correct: false },
+            { option: "Milhares", correct: false },
+            { option: "Infinitas", correct: true },
+        ],
+    },
+    {
+        question: "Qual o número mínimo de jogadores em cada time numa partida de futebol?",
+        answers: [
+            { option: "7", correct: false },
+            { option: "5", correct: false },
+            { option: "8", correct: true },
+        ],
+    },
+
+    {
+        question: " Qual o maior animal terrestre?",
+        answers: [
+            { option: "Baleia Azul", correct: false },
+            { option: "Dinossauro", correct: false },
+            { option: "Elefante africano", correct: true },
+        ],
+    },
+
+    {
+        question: "Quanto é 5 x 5?",
+        answers: [
+            { option: "25", correct: false },
+            { option: "10", correct: false },
+            { option: "15", correct: true },
+        ],
+    },
+];
